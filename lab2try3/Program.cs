d7be544fb3e4f9618007b939635e1f66c724b125
Odai Al-Obeidat <odai.al.obeidat950@gmail.com>
2023-06-14 11:32:57 +0300

    Add project files.

---
+++ lab2try3/Program.cs
@@ -0,0 +1,126 @@
+﻿
+using System;
+using System.Reflection;
+
+namespace ATM
+{
+    public class Program
+
+    {
+
+        public static decimal Balance = 100;
+        static void Main(string[] args)
+        {
+
+            UserInterface();
+        }
+
+
+        public static void UserInterface()
+        {
+            UserInterface(Balance);
+        }
+
+
+
+        public static decimal ViewBalance()
+        {
+
+            return Balance;
+        }
+
+        public static decimal WithDraw(decimal amount)
+        {
+            if (Balance < Convert.ToInt32(amount))
+            {
+                Console.WriteLine("Your bank balance is not enough to make this withdrawal.");
+            }
+            else if (Convert.ToInt32(amount) < 0)
+            {
+                Console.WriteLine("You are trying to withdraw a cash amount of less than zero.");
+            }
+
+
+            else
+            {
+                Balance -= Convert.ToInt32(amount);
+                Console.WriteLine($"An amount of {amount}$ has been withdrawn from your account, and the remaining amount is {Balance}$.");
+            };
+            return Balance;
+        }
+
+
+        public static decimal Deposit(decimal amount)
+        {
+            //decimal value = Convert.ToDecimal(Console.ReadLine());
+            if (0 > amount)
+            {
+                Console.WriteLine("You are trying to deposit a cash amount less than zero");
+            }
+            else
+            {
+                Balance += Convert.ToInt32(amount);
+                Console.WriteLine($"Your bank account has been credited with {amount}$, your balance is now {Balance}$");
+
+            }
+            return Balance;
+        }
+
+
+
+
+        public static void UserInterface(decimal Balance)
+        {
+
+            string transaction = "";
+
+            while (true)
+            {
+
+                Console.WriteLine("Choose a transaction: 1) ViewBalance,  2) WithDraw,  3) Deposit,  4) Exit");
+                transaction = Console.ReadLine();
+
+
+                if (transaction == "1")
+                {
+                    Console.WriteLine($"Your balance is {ViewBalance()}$");
+                }
+                else if (transaction == "2")
+                {
+                    Console.WriteLine("Enter amount to withdraw:");
+                    decimal amount = Convert.ToDecimal(Console.ReadLine());
+                    WithDraw(amount);
+
+                }
+                else if (transaction == "3")
+                {
+                    Console.WriteLine("Enter amount to deposit:");
+                    decimal amount = Convert.ToDecimal(Console.ReadLine());
+                    Deposit(amount);
+                }
+                else if (transaction == "4")
+                {
+                    Console.WriteLine("Thank you for using our ATM!");
+                    break;
+                }
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ testlab2try3/UnitTest1.cs
@@ -0,0 +1,82 @@
+
+using ATM;
+using System.Security.Principal;
+using Xunit;
+
+namespace ATMtest
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void TestViewBalance()
+        {
+            // Arrange
+            decimal TestBalance1 = Program.Balance;
+            decimal actual;
+
+
+            // Act
+            actual = Program.ViewBalance();
+
+            // Assert
+            Assert.Equal(TestBalance1, actual);
+        }
+
+        [Theory]
+        [InlineData(-50)]
+        [InlineData(200)]
+        [InlineData(50)]
+        public void TestWithDraw(decimal amount)
+        {
+            // Arrange
+            decimal TestBalance2 = 100;
+            Program.Balance = TestBalance2;
+
+            // Act
+            decimal result = Program.WithDraw(amount);
+
+            // Assert
+            if (amount < 0)
+            {
+                Assert.Equal(TestBalance2, result);
+
+            }
+            else if (TestBalance2 < amount)
+            {
+                Assert.Equal(TestBalance2, result);
+
+            }
+            else
+            {
+                Assert.Equal(TestBalance2 - amount, result);
+
+            }
+        }
+
+        [Theory]
+        [InlineData(50)]
+        [InlineData(-40)]
+        public void TestDeposit(decimal amount2)
+        {
+            // Arrange
+            decimal TestBalance3 = 100;
+            Program.Balance = TestBalance3;
+
+            // Act
+            decimal result2 = Program.Deposit(amount2);
+
+            if (amount2 < 0)
+            {
+                Assert.Equal(TestBalance3, result2);
+
+            }
+            else
+            {
+                Assert.Equal(TestBalance3 + amount2, result2);
+            }
+        }
+    }
+}
+
+
+
